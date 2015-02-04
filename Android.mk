5d1337162b685221a1181bfe5b0ade5bfd7ef5aa
Robin Adams <robin.adams@codeaurora.org>
2015-02-04 15:47:29 -0800

    sepolicy: Add qm8626-led and qm8626-usb SElinux rules
    Added policy for QM8626 specific features, init needs to start
    LED and USB setup apps and these apps need access to sysfs
    
    Change-Id: Ica7fe7b558fc27118a49d61eb858409bd3dee92d

--- Android.mk
+++ Android.mk
@@ -95,7 +95,9 @@ BOARD_SEPOLICY_UNION := \
        kernel.te \
        vold.te \
        wfdservice.te \
-       usf.te
+       usf.te \
+       qm8626-led.te \
+       qm8626-usb.te
 
 # Compile sensor policy only for SSC targets
 SSC_TARGET_LIST := apq8084
