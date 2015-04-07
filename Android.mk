e327c719094d230607606b6447038766de7e10b1
Dheeraj Shetty <dshetty@codeaurora.org>
2015-04-06 18:03:21 -0700

    Add policy to allow radio processes to use qmux_radio.
    Change-Id: I0eff0d714889b5e9d37d37fc2ad53c352188ca0a

--- Android.mk
+++ Android.mk
@@ -85,7 +85,8 @@ BOARD_SEPOLICY_UNION := \
        hbtp.te \
        kernel.te \
        vold.te \
-       clockwork_proxy.te
+       clockwork_proxy.te \
+       radio.te
 
 # Compile sensor pilicy only for SSC targets
 SSC_TARGET_LIST := apq8084
