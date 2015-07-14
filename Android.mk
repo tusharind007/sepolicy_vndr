omnirom/android_device_qcom_sepolicy

WFD: SELinux policies for native wfdservice
Define domain polices for WFD service domain

CRs-Fixed: 781774
Change-Id: Ic6aeddba07e96cac9e2a8c8591d63e06960af9f2

Android.mk
@@ -92,7 +92,8 @@ BOARD_SEPOLICY_UNION := \
        mediaserver_test.te \
        hbtp.te \
        kernel.te \
-       vold.te
+       vold.te \
+       wfdservice.te
 
 # Compile sensor pilicy only for SSC targets
 SSC_TARGET_LIST := apq8084

tusharind007/sepolicy_vndr

Android.mk
@@ -97,7 +97,8 @@ BOARD_SEPOLICY_UNION := \
        mediaserver_test.te \
        hbtp.te \
        dun-server.te \
-       sapd.te
+       sapd.te \
+       wfdservice.te
 
 # Compile sensor pilicy only for SSC targets
 SSC_TARGET_LIST := apq8084