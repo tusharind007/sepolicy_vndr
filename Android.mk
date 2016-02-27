56ed2a1859c2751d5d5ec8cdb9701847c647cd43
Roopesh Rajashekharaiah Nataraja <roopes@codeaurora.org>
2016-02-26 17:35:32 -0800

    sepolicy: Modify makefile to point to new sepolicy project path
    Change-Id: I9a239637c1540c2898060894bd0929097b10c191

--- Android.mk
+++ Android.mk
@@ -1,9 +1,9 @@
 # Board specific SELinux policy variable definitions
 ifeq ($(call is-vendor-board-platform,QCOM),true)
-BOARD_SEPOLICY_DIRS := \
-       $(BOARD_SEPOLICY_DIRS) \
-       device/qcom/sepolicy \
-       device/qcom/sepolicy/common \
-       device/qcom/sepolicy/test \
-       device/qcom/sepolicy/$(TARGET_BOARD_PLATFORM)
+    BOARD_SEPOLICY_DIRS := \
+        $(BOARD_SEPOLICY_DIRS) \
+        $(BOARD_SEPOLICY_8909W_DIR) \
+        $(BOARD_SEPOLICY_8909W_DIR)/common \
+        $(BOARD_SEPOLICY_8909W_DIR)/test \
+        $(BOARD_SEPOLICY_8909W_DIR)/$(TARGET_BOARD_PLATFORM)
 endif
