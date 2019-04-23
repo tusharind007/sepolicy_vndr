c02bfa8d504ffaa5209d4ed095665a3d79163bec
Arun George <arungeor@codeaurora.org>
2019-04-23 15:50:25 +0530

    sepolicy: updated the security policy for ssgtzd daemon
    The tz daemon process ssgtzd was not getting started during init process.
    Security policy changes are made to enable the same.
    
    Change-Id: I3898b483ffc5a136591a9343712a195c5006f869

--- Android.mk
+++ Android.mk
@@ -6,7 +6,8 @@ BOARD_SEPOLICY_DIRS := \
        $(LOCAL_PATH) \
        $(LOCAL_PATH)/vendor/common \
        $(LOCAL_PATH)/vendor/$(TARGET_BOARD_PLATFORM) \
-       $(LOCAL_PATH)/vendor/common/sysmonapp
+       $(LOCAL_PATH)/vendor/common/sysmonapp \
+       $(LOCAL_PATH)/vendor/common/ssg
 
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR := \
     $(BOARD_PLAT_PUBLIC_SEPOLICY_DIR) \
